0cf1366419af8e34776cf086c31c314b7d39e690
Víctor Domínguez <viconel27@gmail.com>
2023-05-27 00:16:38 +0200

    Añadido logger

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,7 @@
+import logging
+import string
+import sys
+from pathlib import Path
 from typing import Collection
 
 
@@ -9,3 +13,29 @@ def inner_contained_fully_outer(inner: Collection, outer: Collection) -> bool:
     :return: true if all elements are present, otherwise false
     """
     return all(element in outer for element in inner)
+
+
+def obtain_logger(name: string):
+    """
+    Builds a logger that writes on stdout and a file placed in logs' folder
+    :param name: Name of the logger
+    :return: Logger ready to use
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    standard_output_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler(Path(__file__).parent / "../../logs" / f'{name}.log')
+
+    formatter = logging.Formatter(
+        '[%(asctime)s.%(msecs)d]\t  %(name)s - %(levelname)s \t[%(name)s.%(funcName)s:%(lineno)d]\t %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
+
+    standard_output_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(standard_output_handler)
+    logger.addHandler(file_handler)
+
+    return logger
