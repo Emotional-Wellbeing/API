d65bd8081dce915834136193d8f455f7ac5890af
Víctor Domínguez <viconel27@gmail.com>
2023-05-26 11:10:17 +0200

    Añadido los test de response (build_user_data_response) y reformateados los test tanto de response como de validator

--- tests/test_user_data_validator.py
+++ tests/test_user_data_validator.py
@@ -7,61 +7,53 @@ from src.validator.user_data_validator import UserDataValidator
 class TestUserDataValidator(unittest.TestCase):
     def setUp(self) -> None:
         self.udv = UserDataValidator()
-        self.basepath = Path(__file__).parent / '../json'
+        self.base_path = Path(__file__).parent / '../json/input'
+
+    def __doTrueTest(self, input_path, text):
+        with input_path.open() as json_file:
+            input_data = json.load(json_file)
+        self.assertTrue(self.udv.validate(input_data), text)
+
+    def __doFalseTest(self, input_path, text):
+        with input_path.open() as json_file:
+            input_data = json.load(json_file)
+        self.assertFalse(self.udv.validate(input_data), text)
 
     def testValidatorNoUserId(self):
-        path = self.basepath / 'user_data_no_user_id.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertFalse(self.udv.validate(data), "No user id provided")
+        path = self.base_path / 'user_data_no_user_id.json'
+        self.__doFalseTest(path, "No user id provided")
 
     def testValidatorEmptyData(self):
-        path = self.basepath / 'user_data_empty_data.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertTrue(self.udv.validate(data), "No data provided")
+        path = self.base_path / 'user_data_empty_data.json'
+        self.__doTrueTest(path, "No data provided")
 
     def testValidatorFullData(self):
-        path = self.basepath / 'user_data_full_data.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertTrue(self.udv.validate(data), "Full data provided")
+        path = self.base_path / 'user_data_full_data.json'
+        self.__doTrueTest(path, "Full data provided")
 
     def testValidatorOneData(self):
-        path = self.basepath / 'user_data_one_data.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertTrue(self.udv.validate(data), "One data provided")
+        path = self.base_path / 'user_data_one_data.json'
+        self.__doTrueTest(path, "One data provided")
 
     def testValidatorWrongFirstLevel(self):
-        path = self.basepath / 'user_data_wrong_first_level.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertFalse(self.udv.validate(data), "Wrong data provided at first level")
+        path = self.base_path / 'user_data_wrong_first_level.json'
+        self.__doFalseTest(path, "Wrong data provided at first level")
 
     def testValidatorWrongSecondLevel(self):
-        path = self.basepath / 'user_data_wrong_second_level.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertFalse(self.udv.validate(data), "Wrong data provided at second level")
+        path = self.base_path / 'user_data_wrong_second_level.json'
+        self.__doFalseTest(path, "Wrong data provided at second level")
 
     def testValidatorWrongThirdLevelLeave(self):
-        path = self.basepath / 'user_data_wrong_third_level_leave.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertFalse(self.udv.validate(data), "Wrong data provided at third level")
+        path = self.base_path / 'user_data_wrong_third_level_leave.json'
+        self.__doFalseTest(path, "Wrong data provided at third level")
 
     def testValidatorWrongThirdLevelNonLeave(self):
-        path = self.basepath / 'user_data_wrong_third_level_non_leave.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertFalse(self.udv.validate(data), "Wrong data provided at third level")
+        path = self.base_path / 'user_data_wrong_third_level_non_leave.json'
+        self.__doFalseTest(path, "Wrong data provided at third level")
 
     def testValidatorWrongFourthLevel(self):
-        path = self.basepath / 'user_data_wrong_fourth_level.json'
-        with path.open() as json_file:
-            data = json.load(json_file)
-        self.assertFalse(self.udv.validate(data), "Wrong data provided at fourth level")
+        path = self.base_path / 'user_data_wrong_fourth_level.json'
+        self.__doFalseTest(path, "Wrong data provided at fourth level")
 
 
 if __name__ == '__main__':
