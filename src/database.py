85771e63444b126e540cb0e90dee71ec85e522c8
Víctor Dominguez <viconel27@gmail.com>
2023-06-16 19:39:25 +0200

    Cambiada las inserciones en la base de datos para facilitar las consultas

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ import threading
 from typing import Dict
 
 from pymongo import MongoClient
-from pymongo.results import InsertOneResult
+from pymongo.results import InsertOneResult, InsertManyResult
 
 
 class Database:
@@ -51,21 +51,41 @@ class Database:
         return collection.insert_one(data)
 
     @staticmethod
-    def insert_daily_questionnaires(data: Dict) -> InsertOneResult:
+    def insert_daily_questionnaires(data: Dict) -> InsertManyResult:
         """
         Insert data into daily_questionnaires collection
         :param data: data to insert
-        :return: InsertOneResult of the execution
+        :return: InsertManyResult of the execution
         """
         collection = Database._database.get_collection('daily_questionnaires')
-        return collection.insert_one(data)
+
+        processed_data = []
+
+        for key in data["data"]:
+            for element in data["data"][key]:
+                values = element
+                values.update({"type": key, "userId": data["userId"]})
+                values.pop("id")
+                processed_data.append(values)
+        # print(processed_data)
+        return collection.insert_many(processed_data)
 
     @staticmethod
-    def insert_one_off_questionnaires(data: Dict) -> InsertOneResult:
+    def insert_one_off_questionnaires(data: Dict) -> InsertManyResult:
         """
         Insert data into one_off_questionnaires collection
         :param data: data to insert
-        :return: InsertOneResult of the execution
+        :return: InsertManyResult of the execution
         """
         collection = Database._database.get_collection('one_off_questionnaires')
-        return collection.insert_one(data)
+
+        processed_data = []
+
+        for key in data["data"]:
+            for element in data["data"][key]:
+                values = element
+                values.update({"type": key, "userId": data["userId"]})
+                values.pop("id")
+                processed_data.append(values)
+        # print(processed_data)
+        return collection.insert_many(processed_data)
